c76c297747a603b071db792d5ff496799e27d2c9
SwordXofXDk <leandrojerse@gmail.com>
2025-02-28 20:55:01 -0300

    Now Read me Works

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,21 @@ function toggleMenu(){
         nav.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const readMoreButtons = document.querySelectorAll('.read-more');
+
+    readMoreButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            const serviceText = this.previousElementSibling; 
+            if (serviceText.classList.contains('expanded')) {
+                serviceText.classList.remove('expanded');
+                this.textContent = 'Read More';
+            } else {
+                serviceText.classList.add('expanded');
+                this.textContent = 'Read Less';
+            }
+        });
+    });
+});
